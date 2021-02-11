5b829bb7135a040afc3bdca5a6e0c9dad4d99244
Timittra Islam Hridi <timittra.cse.aust@gmail.com>
2021-02-12 01:18:41 +0600

    last

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const displaySongs = songs => {
     const songContainer = document.getElementById('song-container');
     songContainer.innerHTML = ''; 
     songs.forEach(song => {
+        console.log(song);
         const songDiv = document.createElement('div');
         songDiv.className = "single-result row align-items-center my-3 p-3";
         songDiv.innerHTML = `
