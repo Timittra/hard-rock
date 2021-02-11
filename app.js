9588171eaa6fd3c5b0eac91196a6aa200208a8bf
Timittra Islam Hridi <61421064+Timittra@users.noreply.github.com>
2021-02-12 01:15:50 +0600

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const displaySongs = songs => {
       <div class="col-md-9">
              <h3 class="lyrics-name">${song.title}</h3>
              <p class="author lead">Album by <span>${song.artist.name}</span></p>
-             <audio controls autoplay='false'>
+             <audio controls autoplay>
                 <source src="${song.preview}" type="audio/mpeg">
              </audio>
       </div>
